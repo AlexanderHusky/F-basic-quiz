5a10fdbf1dc80d030b5e41eaf302435925db6cfb
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-10-22 23:07:54 +0800

    [mengqiu] add feedback comments

--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
 import "./style/index.scss";
+// TODO GTB-完成度: - 完成度低，没有实现使用原生DOM更新页面的功能
--- src/javaScript/getData.js
+++ src/javaScript/getData.js
@@ -1,4 +1,5 @@
 const url = "localhost:8080/users/1";
+// TODO GTB-知识点: - Promise的使用不正确：这里应该是返回promise就好了，然后在调用这个api的地方使用async/await
 const getUser = async (id) => {
   return fetch(`${url}/${id}`, {
     headers: {
@@ -9,5 +10,6 @@ const getUser = async (id) => {
     return res.json();
   });
 };
+// TODO GTB-知识点: - 这个module应该是用来定义和导出方法，而不是调用这个方法
 getUser(1);
 // export { getUser };
